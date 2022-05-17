0d1d3b933ca4043f9590071a30a1fb476f8b1d8b
CarolinaQuintoman <quintoman.carolina@gmail.com>
2022-05-16 21:22:17 -0300

    Update CardWidgest.js

--- src/components/CardWidgest/CardWidgest.js
+++ src/components/CardWidgest/CardWidgest.js
@@ -1,13 +1,12 @@
 import React from 'react'
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { faCartShopping} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping} from '@fortawesome/free-solid-svg-icons'
 
 const CardWidgest = () => {
   return (
     <nav className="navbar">
         <h3 className="nav-link" href="#carrito" style={{fontSize:'20px', color:'rgb(189 198 38)'}}>
-            {/* <FontAwesomeIcon /> 2  */}
-            </h3>
+            <FontAwesomeIcon icon={faCartShopping}/> 2 </h3>
     </nav>
 
   )
